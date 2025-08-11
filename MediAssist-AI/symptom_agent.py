5fca8fa897a0c92f604fa32859cab695f4c3076d
Y-Bhatnagar <yashbhatnagar08@gmail.com>
2025-08-11 06:32:27 +0000

    Added venv and wrote a code to summarise symptom

--- MediAssist-AI/symptom_agent.py
+++ MediAssist-AI/symptom_agent.py
@@ -4,8 +4,40 @@ import os # allows to interact with the operating system
 
 #checking if the key is available in envornment
 if not os.getenv("AZURE_INFERENCE_CREDENTIAL"):
-    os.environ ["AZURE_INFERENCE_CREDENTIAL"]== getpass.getpass("Enter your Azure API key: ")
+    os.environ ["AZURE_INFERENCE_CREDENTIAL"]= getpass.getpass("Enter your Azure API key: ")
 
 #ckecking if the endpoint is available in the enviornment
 if not os.getenv("AZURE_INFERENCE_ENDPOINT"):
-    os.environ ["AZURE_INFERENCE_ENDPOINT"]== getpass.getpass("Enter your model: ")
+    os.environ ["AZURE_INFERENCE_ENDPOINT"]= getpass.getpass("Enter your model: ")
+
+#istantiation
+from langchain_azure_ai.chat_models import AzureAIChatCompletionsModel
+assist = AzureAIChatCompletionsModel(
+    model_name="gpt-5-nano",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
+
+#using Lagchain to gather symptoms
+from langchain_core.prompts import ChatPromptTemplate
+prompt = ChatPromptTemplate.from_message(
+    [
+        (
+        "system",
+        "You are a medical proffesional. You task is to have a converstaion with the user to gather the information \
+        regarding their symptoms, and then briefly summarise user's symptoms without lossing any information.\
+        The summmary of information should be in format such that I can use it in embedding.\n\
+        The users symptoms are {input}"
+        ),
+        ("user","{input}"),
+    ]
+)
+
+#using chain
+summary_sym = prompt | assist
+symptoms = summary_sym.invoke (
+    {"input": input("Please tell me your symptoms:\n")}
+)
+print (symptoms)
